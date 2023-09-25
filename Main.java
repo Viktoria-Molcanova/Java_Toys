83ed23eb4027658975ac27a0e230b57d67252e49
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2023-09-25 21:56:25 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Lottery r = new Lottery();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("""
+
+                    Main menu:
+                    1. Добавить игрушку в призовой фонд
+                    2. Изменить частоту выпадения игрушки
+                    3. Розыгрыш призов 
+                    z. Выход
+                    >\s""");
+            var selection = sc.next();
+            switch (selection) {
+                case "1" -> r.addToy();
+                case "2" -> r.setFrequency();
+                case "3" -> r.Lottery();
+                case "z" -> {
+                    System.exit(0);
+                }
+                default -> System.out.println("Ошибка ввода. Попробуйте снова");
+            }
+        }
+    }
+}

220649fed112b3d90de818b5406cb68e24d5ed2a
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2023-09-25 22:21:06 +0700

    Update Toy.java

--- Toy.java
+++ Toy.java
@@ -1,10 +1,11 @@
 import java.util.Objects;
 
 public class Toy implements Comparable<Toy> {
-
+    
+    private int toyVictoryFrequency;
     private int toyId;
     private String toyTitle;
-    private int toyVictoryFrequency;
+    
 
     public Toy(int toyId, String toyTitle, int toyVictoryFrequency) {
         this.toyId = toyId;
